a6812defb6df9eac142299e2352a3a8f44306a3f
Jade <jade.applegate@gmail.com>
2014-05-03 13:12:39 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GroupCamp::Application.config.secret_key_base = 'b002cfe5c713c027d7eec05c7dd31fcc3260f337e963496bd06c830cd8d15b20e992658043c833fc0da8369e0ffa07a16e993e5251d2b06d960e687fe76bddd1'
