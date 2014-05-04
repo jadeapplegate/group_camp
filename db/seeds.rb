53ca175fb92985ed9f3b7109630e407d6ba3a5f3
Jade <jade.applegate@gmail.com>
2014-05-04 10:48:19 -0700

    re-added seed data

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+User.delete_all
+
+colt = User.create(name: "Colt",  email: "colt@email.com", password: "password", password_confirmation: "password")
+jackie = User.create(name: "Jackie", email: "jackie@email.com", password: "password", password_confirmation: "password")
+tim = User.create(name: "Tim", email: "tim@email.com", password: "password", password_confirmation: "password")
