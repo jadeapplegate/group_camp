33d8c3bf83d2fa180b7e58fa3e959b2c429eceee
Jade <jade.applegate@gmail.com>
2014-05-05 16:52:30 -0700

    added trip controller methods trip views, and updated routes

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,5 +1,5 @@
 class TripsController < ApplicationController
-  before_action :set_trip, only: [:show, :edit, :update, :destroy]
+  # before_action :set_trip, only: [:show, :edit, :update, :destroy]
 
   # GET /trips
   def index
@@ -20,7 +20,6 @@ class TripsController < ApplicationController
   end
 
   # POST /trips
-  # POST /trips.json
   def create
     @trip = Trip.new(trip_params)
 
@@ -36,7 +35,6 @@ class TripsController < ApplicationController
   end
 
   # PATCH/PUT /trips/1
-  # PATCH/PUT /trips/1.json
   def update
     respond_to do |format|
       if @trip.update(trip_params)
@@ -61,9 +59,9 @@ class TripsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_trip
-      @trip = Trip.find(params[:id])
-    end
+    # def set_trip
+    #   @trip = Trip.find(params[:id])
+    # end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def trip_params
--- config/routes.rb
+++ config/routes.rb
@@ -10,40 +10,43 @@ GroupCamp::Application.routes.draw do
 
   resources :users, only: [:new, :create]
 
-  get 'trip/search', to: 'trips#search'
+  get 'trips/search', to: 'trips#search'
 
-  get '/trip/results', to: 'trips#results'
+  get '/trips/results', to: 'trips#results'
 
-  get '/trip/details', to: 'trips#details'
+  get '/trips/details', to: 'trips#details'
   
   resources :trips
 
   resources :guests, only: [:new, :create]
 
-  # every action below is a method in the corresponding controller. Ony the GET actions will have views (index, show, new, edit)
+  #will need another path for the shared url. Maybe like /share/random_generated_string
 
-#         root GET    /                         home#index
-#     sessions POST   /sessions(.:format)       sessions#create
-#  new_session GET    /sessions/new(.:format)   sessions#new
-#      session DELETE /sessions/:id(.:format)   sessions#destroy
-#       signup GET    /signup(.:format)         users#new
-#      signout DELETE /signout(.:format)        sessions#destroy
-#       signin GET    /signin(.:format)         sessions#new
-#        users POST   /users(.:format)          users#create
-#     new_user GET    /users/new(.:format)      users#new
-#  trip_search GET    /trip/search(.:format)    trips#search
-# trip_results GET    /trip/results(.:format)   trips#results
-# trip_details GET    /trip/details(.:format)   trips#details
-#        trips GET    /trips(.:format)          trips#index
-#              POST   /trips(.:format)          trips#create
-#     new_trip GET    /trips/new(.:format)      trips#new
-#    edit_trip GET    /trips/:id/edit(.:format) trips#edit
-#         trip GET    /trips/:id(.:format)      trips#show
-#              PATCH  /trips/:id(.:format)      trips#update
-#              PUT    /trips/:id(.:format)      trips#update
-#              DELETE /trips/:id(.:format)      trips#destroy
-#       guests POST   /guests(.:format)         guests#create
-#    new_guest GET    /guests/new(.:format)     guests#new
+  # every action below is a method in the corresponding controller. Ony the GET actions will have views (index, show, new, edit)
+#       Prefix Verb   URI Pattern               Controller#Action
+#          root GET    /                         home#index
+#      sessions POST   /sessions(.:format)       sessions#create
+#   new_session GET    /sessions/new(.:format)   sessions#new
+#       session DELETE /sessions/:id(.:format)   sessions#destroy
+#        signup GET    /signup(.:format)         users#new
+#       signout DELETE /signout(.:format)        sessions#destroy
+#        signin GET    /signin(.:format)         sessions#new
+#         users POST   /users(.:format)          users#create
+#      new_user GET    /users/new(.:format)      users#new
+#  trips_search GET    /trips/search(.:format)   trips#search
+# trips_results GET    /trips/results(.:format)  trips#results
+# trips_details GET    /trips/details(.:format)  trips#details
+#         trips GET    /trips(.:format)          trips#index
+#               POST   /trips(.:format)          trips#create
+#      new_trip GET    /trips/new(.:format)      trips#new
+#     edit_trip GET    /trips/:id/edit(.:format) trips#edit
+#          trip GET    /trips/:id(.:format)      trips#show
+#               PATCH  /trips/:id(.:format)      trips#update
+#               PUT    /trips/:id(.:format)      trips#update
+#               DELETE /trips/:id(.:format)      trips#destroy
+#        guests POST   /guests(.:format)         guests#create
+#     new_guest GET    /guests/new(.:format)     guests#new
+
 
 
 end
