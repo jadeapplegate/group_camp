89fade139356746456a85749899cd176e68836b0
Jade <jade.applegate@gmail.com>
2014-05-05 21:13:16 -0700

    controller cleanup and added some comments to trips/show

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,5 +1,4 @@
 class TripsController < ApplicationController
-  # before_action :set_trip, only: [:show, :edit, :update, :destroy]
 
   # GET /trips
   def index
@@ -8,15 +7,12 @@ class TripsController < ApplicationController
 
   # GET /trips/1
   def show
+    @trip = Trip.find params[:id]
   end
 
   # GET /trips/new
   def new
     @trip = Trip.new
-  end
-
-  # GET /trips/1/edit
-  def edit
   end
 
   # POST /trips
@@ -26,23 +22,20 @@ class TripsController < ApplicationController
     respond_to do |format|
       if @trip.save
         format.html { redirect_to @trip, notice: 'Trip was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @trip }
       else
         format.html { render action: 'new' }
-        format.json { render json: @trip.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PATCH/PUT /trips/1
   def update
+    @trip = Trip.find params[:id]
     respond_to do |format|
       if @trip.update(trip_params)
         format.html { redirect_to @trip, notice: 'Trip was successfully updated.' }
-        format.json { head :no_content }
       else
         format.html { render action: 'edit' }
-        format.json { render json: @trip.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -50,21 +43,16 @@ class TripsController < ApplicationController
   # DELETE /trips/1
   # DELETE /trips/1.json
   def destroy
+    @trip = Trip.find params[:id]
     @trip.destroy
     respond_to do |format|
       format.html { redirect_to trips_url }
-      format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    # def set_trip
-    #   @trip = Trip.find(params[:id])
-    # end
-
     # Never trust parameters from the scary internet, only allow the white list through.
-    def trip_params
-      params.require(:trip).permit(:arrival, :departure, :camp_info, :share_url)
-    end
+    # def trip_params
+    #   params.require(:trip).permit(:arrival, :departure, :camp_info, :share_url)
+    # end
 end
