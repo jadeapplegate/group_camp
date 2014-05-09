d20bca1bc2f0bb0c1f6d2a07316d5fbb71a90ae7
Jade <jade.applegate@gmail.com>
2014-05-08 19:25:48 -0700

    rspec for requests in progress

--- spec/requests/trips_spec.rb
+++ spec/requests/trips_spec.rb
@@ -2,15 +2,36 @@ require 'spec_helper'
 
 describe "Trips" do
 
-  # new_trip_path
-  describe "GET new" do
+  # trips_search_path
+  describe "GET trips/search" do
     it "should be a 200 response" do
-      get new_trip
+      get "/trips/search"
       response.status.should be(200)
     end
   end
+
+  # trips_results_path
+  describe "GET trips/results" do
+    it "should be a 200 response" do
+      get "/trips/results"
+      response.status.should be(200)
+    end
+  end
+  
 end
 
+
+
+
+#   describe "Home Index" do
+
+#     describe "GET /" do
+#     it "should be a 200 response" do
+#       get "/"
+#       response.status.should be(200)
+#     end
+#   end
+# end
 
 # trips_search  GET    /trips/search(.:format)    trips#search
 # trips_results GET    /trips/results(.:format)    trips#results
@@ -18,12 +39,3 @@ end
 #         trips POST   /trips(.:format)            trips#create
 #               DELETE /trips/:id(.:format)        trips#destroy
 #          trip GET    /trips/:share_url(.:format) trips#show
-
-
-# # new_trip_path
-#   describe "GET new" do
-#     it 'should be a 200 response' do
-#       get :new
-#       expect(response.status).to eq(200)
-#     end
-#   end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,19 @@
-require 'spec_helper'
+# require 'spec_helper'
 
-describe "Users" do
-  describe "GET /" do
-    it "works! (now write some real specs)" do
-      get users_path
-      response.status.should be(200)
-    end
-  end
-end
+# describe "Users" do
+
+#   describe "GET /" do
+#     it "should be a 200 response" do
+#       get :index
+#       response.status.should be(200)
+#     end
+#   end
+# end
+
+
+# describe "GET index" do
+#     it 'should be a 200 response' do
+#       get :index
+#       expect(response.status).to eq(200)
+#     end
+#   end
