db8f182ab4fa9677a6a838c5a2ae5f16d5c09b44
Jade <jade.applegate@gmail.com>
2014-05-12 11:23:48 -0700

    updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-User.delete_all
+# User.delete_all
 
 colt = User.create(name: "Colt",  email: "colt@email.com", password: "password", password_confirmation: "password")
 jackie = User.create(name: "Jackie", email: "jackie@email.com", password: "password", password_confirmation: "password")
